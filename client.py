ac49ea80a824684e3f9904acb2744cb37d855c48
anastasia.maidacenco <maidacenco100@gmail.com>
2023-04-05 22:17:27 +0300

    Initial commit

---
+++ client.py
@@ -0,0 +1,83 @@
+import socket
+import sys
+
+# define server address and port
+SERVER_ADDRESS = 'localhost'
+SERVER_PORT = 5555
+
+# create socket object
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# connect to server
+try:
+    client_socket.connect((SERVER_ADDRESS, SERVER_PORT))
+except:
+    print("Unable to connect to server.")
+    sys.exit()
+
+# receive initial board state from server
+start_msg = client_socket.recv(1024).decode()
+if start_msg != "START":
+    print("Unexpected message received from server.")
+    sys.exit()
+board_str = client_socket.recv(1024).decode()
+#board = list(board_str)
+board = eval(board_str)
+# print initial board state
+print("Tic Tac Toe Game")
+print("-----------------")
+print(" 0 | 1 | 2 ")
+print("-----------")
+print(" 3 | 4 | 5 ")
+print("-----------")
+print(" 6 | 7 | 8 ")
+print("-----------------")
+print_board = board#[str(i) if board[i] == " " else board[i] for i in range(len(board))]
+print(
+    f"Current Board: {print_board[0]}|{print_board[1]}|{print_board[2]}\n              -+-+-\n              {print_board[3]}|{print_board[4]}|{print_board[5]}\n              -+-+-\n              {print_board[6]}|{print_board[7]}|{print_board[8]}")
+
+# loop until game ends
+while True:
+    # check if it is this client's turn
+    turn_msg = client_socket.recv(1024).decode()
+    if turn_msg == "YOUR_TURN":
+        print("Your turn.")
+        # get move from user
+        while True:
+            move = input("Enter move (0-8): ")
+            if move.isdigit() and int(move) >= 0 and int(move) <= 8 and board[int(move)] == " ":
+                break
+            print("Invalid move.")
+        # send move to server
+        client_socket.send(str.encode(move))
+    elif turn_msg == "OTHER_TURN":
+        print("Waiting for other player's move...")
+    elif turn_msg == "GAME_ENDED":
+        print("Game ended.")
+        # receive winner message from server
+        winner_msg = client_socket.recv(1024).decode()
+        if winner_msg == "PLAYER1":
+            print("Player 1 wins!")
+        elif winner_msg == "PLAYER2":
+            print("Player 2 wins!")
+        elif winner_msg == "TIE":
+            print("Tie game.")
+        else:
+            print("Unexpected message received from server.")
+        # close connection and exit
+        client_socket.close()
+        sys.exit()
+    else:
+        print("Unexpected message received from server.")
+        client_socket.close()
+        sys.exit()
+
+    # receive updated board state from server
+    board_str = client_socket.recv(1024).decode()
+    #board = list(board_str)
+    board = eval(board_str)
+
+    # print updated board state
+    print_board = [str(i) if board[i] == " " else board[i] for i in range(len(board))]
+    print(
+        f"Current Board: {print_board[0]}|{print_board[1]}|{print_board[2]}\n              -+-+-\n              {print_board[3]}|{print_board[4]}|{print_board[5]}\n              -+-+-\n              {print_board[6]}|{print_board[7]}|{print_board[8]}")
---
+++ server.py
@@ -0,0 +1,97 @@
+import socket
+
+# define host and port
+HOST = ''
+PORT = 5555
+
+# create socket object
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# bind socket object to host and port
+server_socket.bind((HOST, PORT))
+
+# listen for incoming connections
+server_socket.listen()
+
+print(f"Server is listening on {HOST}:{PORT}")
+
+# define the game board
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+# function to print the game board
+def print_board():
+    print(f"{board[0]}|{board[1]}|{board[2]}")
+    print("-+-+-")
+    print(f"{board[3]}|{board[4]}|{board[5]}")
+    print("-+-+-")
+    print(f"{board[6]}|{board[7]}|{board[8]}")
+
+# function to check if the game has ended
+def game_ended():
+    for i in range(0, 9, 3):
+        if board[i] == board[i+1] == board[i+2] and board[i] != " ":
+            return True
+    for i in range(0, 3):
+        if board[i] == board[i+3] == board[i+6] and board[i] != " ":
+            return True
+    if board[0] == board[4] == board[8] and board[0] != " ":
+        return True
+    if board[2] == board[4] == board[6] and board[2] != " ":
+        return True
+    if " " not in board:
+        return True
+    return False
+
+# function to play the game
+def play_game(conn1, conn2):
+    # send initial board state to clients
+    conn1.send("START".encode())
+    conn1.send(str.encode(str(board)))
+    conn2.send("START".encode())
+    conn2.send(str.encode(str(board)))
+
+    # alternate turns between players
+    current_player = conn1
+    while not game_ended():
+        other_player = conn1 if current_player == conn2 else conn2
+        current_player.send("YOUR_TURN".encode())
+        other_player.send("OTHER_TURN".encode())
+
+        # receive move from current player
+        move = current_player.recv(1024).decode()
+        move = int(move)
+
+        # update board state
+        board[move] = "X" if current_player == conn1 else "O"
+
+        # send updated board state to both clients
+        conn1.send(str.encode(str(board)))
+        conn2.send(str.encode(str(board)))
+
+        # switch to other player's turn
+        current_player, other_player = other_player, current_player
+
+    # send game ended message to both clients
+    conn1.send("GAME_ENDED".encode())
+    conn2.send("GAME_ENDED".encode())
+
+    # determine winner or tie
+    if " " not in board:
+        winner = "TIE"
+    else:
+        winner = "PLAYER1" if current_player == conn2 else "PLAYER2"
+
+    # send winner message to both clients
+    conn1.send(str.encode(winner))
+    conn2.send(str.encode(winner))
+
+    # close connections
+    conn1.close()
+    conn2.close()
+
+# accept connections from clients
+conn1, addr1 = server_socket.accept()
+print(f"Player 1 connected from {addr1}")
+conn2, addr2 = server_socket.accept()
+print(f"Player 2 connected from {addr2}")
+play_game(conn1,conn2)
